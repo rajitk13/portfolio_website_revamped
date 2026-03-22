19a5fb0aa090f2f38ff039fd131ce98ac9398a68
Rajit <73546886+rajitk13@users.noreply.github.com>
2026-03-22 20:09:58 +0530

    Production Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,8 @@ export default defineConfig(({mode}) => {
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
+    preview: {
+      allowedHosts : ["rajit.cc"]
+    }
   };
 });
